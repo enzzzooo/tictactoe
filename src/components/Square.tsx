caa4d096deb15c539a89885d302492d836f54a67
Enzzzooo <enzzzooo@proton.me>
2026-09-22 02:03:00 -0500

    working, not pretty

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,4 +1,10 @@
-export default function Square({ value }: { value: string | null }) {
+export default function Square({
+  value,
+  onSquareClick,
+}: {
+  value: string | null;
+  onSquareClick: () => void;
+}) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
